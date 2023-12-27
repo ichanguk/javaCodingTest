f87db1ca88e97b102deb708d980185891df84da0
changukLee <102301656+ichanguk@users.noreply.github.com>
2023-12-27 22:57:27 +0900

    feat : Q1 주석 update

--- chapter3/교점에_별_만들기.java
+++ chapter3/교점에_별_만들기.java
@@ -3,14 +3,16 @@ import java.util.List;
 import java.util.Arrays;
 
 class Solution {
-    private static class Point {
+    // 점을 표현하기 위한 Point class
+    private static class Point {        
         public final long x, y;
         private Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
     }
-    
+
+    // 교점을 반환하는 함수
     private Point intersection(long A, long B, long E, long C, long D, long F) {
         long denominator = A * D - B * C;
         if (denominator == 0) {
@@ -25,7 +27,8 @@ class Solution {
             return null;
         }
     }
-    
+
+    // answer 배열의 크기 조정을 위한 x, y 좌표의 최댓값, 최솟값 구하기
     private Point getMinPoint(List<Point> points) {
         long x = points.get(0).x;
         long y = points.get(0).y;
@@ -56,8 +59,8 @@ class Solution {
     }
     
     public String[] solution(int[][] line) {
-        List<Point> points = new ArrayList<>();
-        for (int i = 0; i < line.length; i++) {
+        List<Point> points = new ArrayList<>();    // 교점을 저장하기 위한 배열
+        for (int i = 0; i < line.length; i++) {    // 모든 직선 쌍에 대해 교점 구함
             for (int j = i + 1; j < line.length; j++) {
                 Point intersectionP = intersection(line[i][0], line[i][1], line[i][2]
                                                 , line[j][0], line[j][1], line[j][2]);
@@ -67,10 +70,10 @@ class Solution {
             }
         }
         
-        Point minP = getMinPoint(points);
+        Point minP = getMinPoint(points);    // 저장된 정수들에 대해 x, y 좌표의 최댓값, 최솟값 구하기
         Point maxP = getMaxPoint(points);
         
-        int width = (int) (maxP.x - minP.x + 1);
+        int width = (int) (maxP.x - minP.x + 1);    // 2차원 배열의 크기 조정정
         int height = (int) (maxP.y - minP.y + 1);
         
         char[][] arr = new char[height][width];
@@ -78,7 +81,7 @@ class Solution {
             Arrays.fill(row, '.');
         }
         
-        for (Point p : points) {
+        for (Point p : points) {            // 별 표시
             int x = (int) (p.x - minP.x);
             int y = (int) (maxP.y - p.y);
             arr[y][x] = '*';
